7c3a640da79acab4e4e73dd2da353f1f4fbefb0c
GUICHARD Benoit <100837994+GeoSupremacy@users.noreply.github.com>
2024-04-08 18:05:25 +0200

    first IMGUI

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -150,25 +150,83 @@ int main()
 
 #pragma endregion
 	*/
-	sf::Window window(sf::VideoMode(800, 600), "My window");
 
-	// run the program as long as the window is open
-	while (window.isOpen())
+	
+
+	// Initialise GLFW
+	if (!glfwInit())
 	{
-		// check all the window's events that were triggered since the last iteration of the loop
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // On veut OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Pour rendre MacOS heureux ; ne devrait pas être nécessaire
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // On ne veut pas l'ancien OpenGL
+
+	// Ouvre une fenêtre et crée son contexte OpenGl
+	GLFWwindow* window; // (Dans le code source qui accompagne, cette variable est globale)
+	window = glfwCreateWindow(900, 900, "OutLiner", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		glfwTerminate();
+		return -1;
 	}
 
-	return 0;
-	/*
+	glfwMakeContextCurrent(window); // Initialise GLEW
+	glfwSwapInterval(1);
+
+
+	glewExperimental = true; // Nécessaire dans le profil de base
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return -1;
+	}
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+	ImGui_ImplOpenGL3_Init();
+	
+	ImGui::StyleColorsDark();
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	
+	
+	do {
+		glClearColor(1, 1, 1, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+	
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+
+		ImGui::ShowDemoWindow();
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+	
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+/*
 	Engine* _engine = new Engine();
 	_engine->Launch();
 	delete _engine;
-	*/
+*/
+	return 0;
+	
+	
+	
 }
